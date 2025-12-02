b2be947b7c2c20a57b55de2802c41a9977e16d50
AashishThapaMagar <AashishThapaMagar@my.unt.edu>
2025-12-01 23:55:51 -0600

    Updated useCurrentOutfitStore.ts file with minimal changes

--- src/store/useCurrentOutfitStore.ts
+++ src/store/useCurrentOutfitStore.ts
@@ -4,11 +4,11 @@ import type { CurrentOutfit } from "../services/recommender";
 interface CurrentOutfitState {
   currentOutfit: CurrentOutfit | null;
   setCurrentOutfit: (outfit: CurrentOutfit | null) => void;
+  reset: () => void;
 }
 
 export const useCurrentOutfitStore = create<CurrentOutfitState>((set) => ({
   currentOutfit: null,
   setCurrentOutfit: (outfit) => set({ currentOutfit: outfit }),
+  reset: () => set({ currentOutfit: null }),
 }));
-
-
